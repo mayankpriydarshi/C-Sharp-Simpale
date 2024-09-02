8ae23a679e4cdeedd75f3684d1634333d5bfb3ab
mayankpriydarshi <mayankpriydarshi10@gmail.com>
2024-09-02 10:00:39 +0530

    1

---
+++ Compile_time_polymorphism.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public void Numberadd(int a, int b)
+	{
+		console.writeline(a + b);
+	}
+	public void Numberadd(int a, int b, int c)
+	{
+		Console.writeline(a + b + c);
+	}
+}
